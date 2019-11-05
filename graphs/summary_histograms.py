8317a92f50e95bd215b54b4b4d417335fcb00e02
KrystianRej <krystian.rej@gmail.com>
2019-11-06 00:48:47 +0100

    "added SI model test file"

--- graphs/summary_histograms.py
+++ graphs/summary_histograms.py
@@ -59,7 +59,7 @@ def create_query_dict(parameters, values):
 
 
 if __name__ == '__main__':
-    with open('db_auth.json') as json_file:
+    with open('../db_auth.json') as json_file:
         auth_data = json.load(json_file)
         client = MongoClient(auth_data['client_string'])
 
